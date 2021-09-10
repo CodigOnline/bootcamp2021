309004fc59eba13b805e5497d655bc8531723bfe
Alvaro <alortegama@gmail.com>
2021-09-10 11:39:36 +0200

    Guardar compra 3

--- node/node_inicial/server/src/controllers/compra.controller.ts
+++ node/node_inicial/server/src/controllers/compra.controller.ts
@@ -8,7 +8,12 @@ export function save(request: express.Request, response: express.Response) {
     const usuarioId = request.params.id;
     const {importe, formaDePago, gastosEnvio, articulos} = request.body;
     service.save(Number(usuarioId), Number(importe), Number(formaDePago), Number(gastosEnvio), articulos)
-    response.json({msg: "Compra guardarda correctamente"})
+        .then(() => {
+            response.json({msg: "Compra creada correctamente"})
+        })
+        .catch((err) => {
+            response.status(500).json({msg: "Error al guardar la compra", err})
+        })
 }
 
 export function findAll(_: express.Request, _1: express.Response) {
--- node/node_inicial/server/src/services/compra.service.ts
+++ node/node_inicial/server/src/services/compra.service.ts
@@ -1,48 +1,61 @@
 import {Compra, CompraModel} from "../database/models/compra.model";
 import {CompraArticulos} from "../database/models/compra_articulos.model";
 import {Articulo, ArticuloModel} from "../database/models/articulo.model";
+import {mysql} from "../database/mysql";
+import {Transaction} from "sequelize";
 
 export class CompraService {
     save(usuarioId: number, importe: number, formaDePago: number, gastosEnvio: number, articulos: CompraArticulo[]) {
-        Compra.create({usuarioId, importe, formaDePago, gastosEnvio})
-            .then((compra: CompraModel) => {
-                articulos.forEach(articulo => {
-                    Articulo.findByPk(articulo.id, {attributes: ['stock']})
-                        .then((articuloBD: ArticuloModel | null) => {
-                            if (articuloBD!.stock >= articulo.cantidad) {
-                                CompraArticulos.create({
-                                    compraId: compra.id,
-                                    articuloId: articulo.id,
-                                    cantidad: articulo.cantidad
-                                }).then(() => {
-                                    console.log("Compra_Articulos creado correctamente");
-                                    Articulo
-                                        .update({stock: (articuloBD!.stock - articulo.cantidad)},
-                                            {where: {id: articulo.id}})
-                                        .then(() => console.log("Artículo actualizado correctamente"))
-                                        .catch((err: Error) => {
-                                            console.log("Error al actualizar el artículo");
-                                            console.log(err);
+        return mysql.transaction((t: Transaction) => {
+            return Compra.create({usuarioId, importe, formaDePago, gastosEnvio}, {transaction: t})
+                .then((compra: CompraModel) => {
+                    return new Promise<string>(async (resolve, reject) => { //RESOLVE HA ACABADO BIEN REJECT HA ACABADO MAL
+                        let error = false;
+                        for (let i = 0; i < articulos.length && !error; i++) {
+                            const articulo = articulos[i];
+                            console.log(`ARTICULO => ${articulo.id}`);
+                            await Articulo.findByPk(articulo.id, {attributes: ['stock'], transaction: t})
+                                .then(async (articuloBD: ArticuloModel | null) => {
+                                    //AQUI TERMINA EL 1R AWAIT
+                                    if (articuloBD!.stock >= articulo.cantidad) {
+                                        await CompraArticulos.create({
+                                            compraId: compra.id,
+                                            articuloId: articulo.id,
+                                            cantidad: articulo.cantidad
+                                        }, {transaction: t}).then(async () => {
+                                            console.log("Compra_Articulos creado correctamente");
+                                            await Articulo
+                                                .update({stock: (articuloBD!.stock - articulo.cantidad)},
+                                                    {where: {id: articulo.id}, transaction: t})
+                                                .then(() => {
+                                                    console.log("Artículo actualizado correctamente")
+                                                })
+                                                .catch((err: Error) => {
+                                                    console.log("Error al actualizar el artículo");
+                                                    console.log(err);
+                                                    error = true;
+                                                    return reject("Error al actualizar el artículo")
+                                                })
                                         })
+                                            .catch((err: Error) => {
+                                                console.log("Error al guardar la compra_articulo");
+                                                console.log(err);
+                                                error = true;
+                                                return reject("Error al guardar la compra_articulo")
+                                            })
+                                    } else {
+                                        console.log(`No hay stock del artículo ${articulo.id}`);
+                                        error = true;
+                                        return reject(`No hay stock del artículo ${articulo.id}`)
+                                    }
                                 })
-                                    .catch((err: Error) => {
-                                        console.log("Error al guardar la compra_articulo");
-                                        console.log(err);
-                                    })
-                            } else {
-                                // DEVOLVER UNA PROMESA CON UN ERROR Y EN ESA TIRAR LA BD HACIA ATRÁS
-                                // ELIMINAR LA COMPRA
-                                // ELIMINAR LOS COMPRAS_ARTICULOS
-                                // SUMAR EL STOCK A LOS PRODUCTOS RESTADOS
-                            }
-                        })
-                })
+                        }
 
-            })
-            .catch((err: Error) => {
-                console.log("Error al guardar la compra");
-                console.log(err);
-            })
+                        return resolve("OK");
+                    })
+                })
+        })
+
     }
 
     findAll() {
